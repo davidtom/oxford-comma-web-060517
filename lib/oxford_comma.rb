52146c3c16644d1bf277428805c18afc5a289bb8
davidtom <davidjtomczyk@gmail.com>
2017-05-20 13:16:21 -0400

    completed lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+def oxford_comma (array)
 
+  puts array.size
+
+  case array.size
+
+  when 1
+    array[0]
+  when 2
+    array.join(" and ")
+  else
+    last_item_index = array.size-1
+    string = String.new
+    array.each_with_index { |item, i|
+      if i == last_item_index
+        string += "and #{item}"
+        puts string
+      else
+        string += "#{item}, "
+        puts string
+      end
+    }
+    string
+  end
 end
